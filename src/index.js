e5a532da040aad45560308635b4821942edd02eb
Varshith Reddy <64683497+varshithreddy28@users.noreply.github.com>
2023-10-13 18:51:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </Provider>
   </React.StrictMode>
 );
